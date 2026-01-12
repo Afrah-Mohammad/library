9ef596281da2328ac0c3a32fcce8c238cd965b04
Afrah-Mohammad <01fe23bcs200@kletech.ac.in>
2026-01-12 19:47:22 +0530

    Create Feedback schema and model

--- models/Book.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const BookSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  available: { type: Boolean, default: true }
-});
-
-module.exports = mongoose.model("Book", BookSchema);
